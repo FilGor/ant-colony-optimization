74aa1d7dc7fd9b5af0848e5922da8227bbabe7c5
FilGor <k24don@o2.pl>
2021-05-18 11:06:31 +0200

    update route for one ant function (without pheromone)

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ routes=np.matrix([
     [1,0,0,0,0]
 ])
 
+
+
+
 PheromoneValues = np.full((4,4),1)
 visibilityMatrix = 1/distanceMatrix             #podzielone przez zero zmienia sie na "inf"
 visibilityMatrix[visibilityMatrix == inf] = 0    #wstawiamy tam zera
@@ -30,21 +33,25 @@ visibilityMatrix[visibilityMatrix == inf] = 0    #wstawiamy tam zera
 
 iteracje =1
 numOfAnts = 1
-
+przejscie =1 #
 
 def find():
+    global przejscie
     actualPropabilities =[]
     for ite in range(iteracje):
         #aktualizacja feromonow
         tempVisibilityMatrix = visibilityMatrix
-        for przejscie in miasta:#przejscie z miasta do miasta
+        for iloscPrzejsc in miasta:#przejscie z miasta do miasta
+            if iloscPrzejsc >= iloscMiast-1: #uwidaczniamy miasto numer 1
+                tempVisibilityMatrix[:,0] = visibilityMatrix[:,0]
+            tempVisibilityMatrix[:, przejscie-1] = 0  # wylaczamy widocznosc dla odwiedzonych miast
             for miasto in miasta: #sprawdzenie drog w aktualnym miescie
                 print("MIASTO:")
                 print(miasto)
                 goraWzoru = (pow(PheromoneValues[przejscie-1, miasto-1], alfa) * pow(tempVisibilityMatrix[przejscie-1, miasto-1],beta))
                 print("GORA WZORU : ")
                 print(goraWzoru)
-                dolWzoru = np.multiply(PheromoneValues[przejscie-1,:], tempVisibilityMatrix[przejscie-1,:])
+                dolWzoru = np.multiply(PheromoneValues[przejscie-1,:], tempVisibilityMatrix[przejscie-1,:]) # dodac alfe i bete
                 dolWzoru[dolWzoru == inf] = 0
                 print("DOL WZORU : ")
                 print(dolWzoru)
@@ -57,27 +64,47 @@ def find():
                 "LIST OF PROBPSPSP"
             )
             print(actualPropabilities)
-            test = np.cumsum(actualPropabilities)
+            cumulativeSum =[]
+            availableCities =[]
+            availableCities.clear()
+            cumulativeSum.clear()
 
-            print(test)
-            for prob in range(len(actualPropabilities)): #wylaczamy widocznosc dla odwiedzonych miast
-                if actualPropabilities[prob] == 0:
-                    tempVisibilityMatrix[przejscie, prob] = 0
-                elif actualPropabilities[prob]< #tofinish
-                    pass
-            r = random.uniform(test[0], test[-1])
+            # liczenie cumulative sum i dodawanie dostepnych miast do listy
+            cumulativeSum.append(1)
+            whileIterator=0
+            while len(actualPropabilities)>0:
+                x = actualPropabilities.pop(0)
+                if x != 0:
+                    cumulativeSum.append(sum(actualPropabilities))
+                    availableCities.append(whileIterator)
+                    whileIterator += 1
+                elif( x == 0 ):  # usuwanie wszystkich zer
+                        whileIterator += 1
+
+                #whileIterator +=1
+            cumulativeSum.pop(-1) #usuniecie nadmiarowego zera
+
+
+            r = random.uniform(0, 1)
+
+
+            tempIterator=0
+            for i in availableCities:
+                if tempIterator == len(availableCities)-1:
+                    routes[0, iloscPrzejsc] = i + 1
+                    przejscie = i + 1  # zmiana aktualnie badanego miasta
+                    break
+                if (cumulativeSum[tempIterator] >= r and r > cumulativeSum[tempIterator+1]):
+                    routes[0,iloscPrzejsc] = i+1
+                    przejscie= i+1   # zmiana aktualnie badanego miasta
+                    break
+                tempIterator += 1
+
+            print(cumulativeSum)
+            print(routes)
+
+
             actualPropabilities.clear()
-
-
-
-
-
-
-
-
-
-
-              
 
 
 
